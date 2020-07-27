775c68d34dca994a1f84eb313790c9af579ac222
長坂太一 <nagasakataichi@Ts-MacBA.local>
2020-07-27 10:08:49 +0900

    complete Q5.

--- 03a_5.rb
+++ 03a_5.rb
@@ -1,35 +1,56 @@
 require "pry"
 
 class User
-  def initialize(params)
-    $name = params[:name]
-    $age = params[:age]
-  end
-end
 
-def info_entry_fee
-  # name = params[:name]
-  # age = params[:age]
-  if 0 <= age <= 5
-    price = @infant
-  elsif 6 <= age <= 12
-    price = @children
-  elsif 13 <= age <= 63
-    price = @adult
-  elsif 64 <= age <= 120
-    price = @senior
-    puts "#{:name}さんの入場料は #{price}¥ 円です。"
+  attr_reader :name, :age
+
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
   end
 end
 
 class Zoo
-  def initialize(params)
+
+    # attr_reader :name, :infant, :children, :adult, :senior
+
+  def initialize(**params)
     @name = params[:name]
     @infant = params[:entry_fee][:infant]
     @children = params[:entry_fee][:children]
     @adult = params[:entry_fee][:adult]
     @senior = params[:entry_fee][:senior]
   end
+
+  # def info_entry_fee(user)
+  #   user_age = user.age
+  #   if 0 <= user_age && user_age <= 5
+  #     price = @infant
+  #     # binding.pry
+  #   elsif 6 <= user_age && user_age <= 12
+  #     price = @children
+  #   elsif 13 <= user_age && user_age  <= 63
+  #     price = @adult
+  #   else 64 <= user_age && user_age <= 120
+  #     price = @senior
+  #   end
+  #   puts "#{user.name}さんの入場料は #{price} 円です。"
+  # end
+
+  def info_entry_fee(user)
+    case user.age
+    when 0..5
+      price = @infant
+    when 6..12
+      price = @children
+    when 13..63
+      price = @adult
+    when 64..120
+      price = @senior
+    end
+    puts "#{user.name}さんの入場料は #{price} 円です。"
+  end
+
 end
 
 zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
